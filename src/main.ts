ce5666e97b1332f2b3e75f50933280e8388b7972
Aman2975 <amandeep2975@gmail.com>
2026-06-10 12:21:11 +0530

    allow access to cors origin

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,14 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+    app.enableCors({
+    origin: '*',                     
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+   app.useGlobalPipes(new ValidationPipe());
+
    const config = new DocumentBuilder()
   .setTitle('Schedula API')
   .setDescription('Doctor Appointment Booking System')
